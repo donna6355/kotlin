76632b1725bcf663c11a933cd88caf8bfa274a96
Donna <donnajun1214@gmail.com>
2022-04-24 13:52:19 +0900

    datatype of numbers

--- app/src/main/java/com/donna6355/kotlinplaygroud/basics.kt
+++ app/src/main/java/com/donna6355/kotlinplaygroud/basics.kt
@@ -5,4 +5,13 @@ fun main(){
     val greeting = "Hi," //immutable
     myName = "Mir"
     println("$greeting $myName")
+
+    val one:Int = 1 // Int
+    val threeBillion:Long = 3000000000 // Long
+    val hundred:Short = 327
+    val oneByte: Byte = 1
+
+    val myFloat:Float = 23.12F
+    val myDouble:Double = 2.2348173984719837498729384
+
 }
