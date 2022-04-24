5f21c833c35f6f76de99c41d0fc6a276a96228e9
Donna <donnajun1214@gmail.com>
2022-04-24 13:43:29 +0900

    kotlin var for mutable n val for immutable

---
+++ app/src/main/java/com/donna6355/kotlinplaygroud/basics.kt
@@ -0,0 +1,8 @@
+package com.donna6355.kotlinplaygroud
+
+fun main(){
+    var myName = "Isaac" //mutable
+    val greeting = "Hi," //immutable
+    myName = "Mir"
+    println("$greeting $myName")
+}
