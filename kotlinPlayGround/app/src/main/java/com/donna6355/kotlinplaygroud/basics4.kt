1a36a3a80d75521d6d2f323de39c00fcb6cc1435
Donna <donnajun1214@gmail.com>
2022-04-25 16:40:58 +0900

    inheritance

--- kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics4.kt
+++ kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics4.kt
@@ -25,4 +25,43 @@ fun main() {
 //    val updatedUserId = updateUser.id
 //    val updatedUserName = updateUser.name
 
+
+    var audi = MyCar("A3","AUDI")
+    var tesla = ElectricCar("S300","TESLA",20.00)
+
+    //Polymorphism
+    //Polymorphism is a mechanism when a behavior can be implemented differently by many classes
+    audi.drive(200.0)
+    tesla.drive(200.0)
+
+    tesla.extendRange(5.0)
+    tesla.drive()
+}
+
+//super class, parent class,base class
+//need 'open' to inherits, otherwise 'sealed' to block inherits
+open class MyCar(val name: String, val brand: String) {
+    open var range: Double = 0.0
+
+    fun extendRange(amount: Double) {
+        if (amount > 0) range += amount
+    }
+
+    open fun drive(distance: Double) {
+        println("drove for $distance km")
+    }
+
+}
+
+//sub class, child class, derived class
+class ElectricCar(name: String, brand: String, battery: Double) : MyCar(name, brand) {
+    override var range = battery * 6
+
+    override fun drive(distance: Double) {
+        println("drove for $distance km on Electricity")
+    }
+
+    fun drive(){
+        println("drove for $range km on Electricity")
+    }
 }
