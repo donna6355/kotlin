efc7e9c86fbb668bf53b6442f2afce941aa48a71
Donna <donnajun1214@gmail.com>
2022-04-28 13:19:37 +0900

    lists

--- kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics5.kt
+++ kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics5.kt
@@ -25,6 +25,30 @@ fun main() {
 
     val mix = arrayOf(Fruit("Apple", 5), "MON", "TUE", 1, "THU", 4, "SAT")
     println(mix.contentToString())
+
+    val months = listOf("January", "February", "March") //immutable list
+    val anyTypes = listOf(1, 2, "hey", 3.33, true)
+
+//    months.add("April") // not works
+
+    val additionalMonth = months.toMutableList()
+    val newMonths = listOf("April", "May", "June")
+    additionalMonth.addAll(newMonths)
+    additionalMonth.add("July")
+    println(additionalMonth)
+
+    val days2 = mutableListOf<String>("Mon", "Tue", "Wed")
+    days2.add("Thu")
+    print(days2)
+    days2[2] = "SATURDAY"
+    print(days2)
+    days2.removeAt(0)
+    val remove = listOf<String>("SATURDAY", "Tue")
+    days2.removeAll(remove) //pass elements to remove
+    print(days2)
+    days2.removeAll(days2)
+    print(days2)
+
 }
 
 data class Fruit(val name: String, val price: Int)
