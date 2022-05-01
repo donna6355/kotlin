513c034a42cf4ace9a8198a048dcbabd8b8d81bb
Donna <donnajun1214@gmail.com>
2022-05-01 12:16:50 +0900

    exception handling_try,catch,finally,throw

--- kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics7.kt
+++ kotlinPlayGround/app/src/main/java/com/donna6355/kotlinplaygroud/basics7.kt
@@ -1,5 +1,8 @@
 package com.donna6355.kotlinplaygroud
 
+import java.lang.ArithmeticException
+import java.lang.NumberFormatException
+
 fun main() {
     println(OuterClass.NestedClass().description)
     var obj = OuterClass.NestedClass()
@@ -10,6 +13,34 @@ fun main() {
     obj2.foo()
 
 
+    //exception handling
+
+
+    val str = getNumber("10.5")
+    println(str)
+    validate(15)
+    println("code after validation check...")
+}
+
+
+fun getNumber(str: String): Int {
+    return try {
+        Integer.parseInt(str)
+    } catch (e: ArithmeticException) {
+        println("ArithmeticException")
+        1
+    } catch (e: NumberFormatException) {
+        println("NumberFormatException")
+        0
+    } finally {
+        println("finally block always executes")
+    }
+}
+
+fun validate(age: Int) {
+    if (age < 18) throw ArithmeticException("under age")
+    //throw is not for productino as it causes crash
+    else println("eligible for drive")
 }
 
 class OuterClass {
