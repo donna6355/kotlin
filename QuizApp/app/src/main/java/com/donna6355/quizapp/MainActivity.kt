524b232350683e2c12f5ce42fba075cdc197e6c3
Donna <donnajun1214@gmail.com>
2022-05-03 13:14:20 +0900

    create new activity and move to next activity by intent

--- QuizApp/app/src/main/java/com/donna6355/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/donna6355/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.donna6355.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        val nameInput: EditText = findViewById(R.id.nameInput)
+
+        btnStart.setOnClickListener {
+            if (nameInput.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent: Intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish() // finish this activity and cannot come back here
+            }
+        }
     }
 }
