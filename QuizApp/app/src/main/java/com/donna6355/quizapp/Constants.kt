4bd98a787d12aca5670082a3f72a7f0bdeef97ec
Donna <donnajun1214@gmail.com>
2022-05-03 13:33:53 +0900

    constant and data class for quiz

---
+++ QuizApp/app/src/main/java/com/donna6355/quizapp/Constants.kt
@@ -0,0 +1,49 @@
+package com.donna6355.quizapp
+
+object Constants {
+    fun getQuestions(): ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Angola", "Austria",
+            "Australia", "Armenia", 3
+        )
+
+        questionsList.add(que2)
+
+
+        val que3 = Question(
+            3, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Belarus", "Belize",
+            "Brunei", "Brazil", 4
+        )
+
+        questionsList.add(que3)
+
+
+        val que4 = Question(
+            4, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas", "Belgium",
+            "Barbados", "Belize", 2
+        )
+
+        questionsList.add(que4)
+        return questionsList
+    }
+}
---
+++ QuizApp/app/src/main/java/com/donna6355/quizapp/Question.kt
@@ -0,0 +1,12 @@
+package com.donna6355.quizapp
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
